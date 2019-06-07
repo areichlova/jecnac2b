0ba20eb64e607cabb052f112f760f0cd6028e9a0
kubik <kubik@PC389.SPSEJECNANET.CZ>
2019-06-07 10:42:37 +0200

    Přidávám třídu Pasovec

---
+++ src/zoo/Pasovec.java
@@ -0,0 +1,41 @@
+package zoo;
+/**
+ * 
+ * @author kubik
+ *
+ */
+public class Pasovec implements AnimalInterface{
+private double weight;
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return weight ;
+	}
+
+	@Override
+	public void setWeight(double weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
